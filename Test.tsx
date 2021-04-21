4cd8b1c97e340898b97f938322776a7859185246
Lukas Leidner <lukas.leidner@reflact.com>
2021-04-21 12:16:58 +0200

    Neu

--- Test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="Test" tilewidth="32" tileheight="32" tilecount="2337" columns="57">
- <image source="../../Desktop/Test.png" width="1824" height="1339"/>
-</tileset>
