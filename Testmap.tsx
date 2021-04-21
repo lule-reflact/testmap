188fc4cf25f9568eb6c171f36e837765537d59a9
Lukas Leidner <lukas.leidner@reflact.com>
2021-04-21 11:51:37 +0200

    Map new

---
+++ Testmap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Testmap" tilewidth="32" tileheight="32" tilecount="2337" columns="57">
+ <image source="Test.png" width="1824" height="1339"/>
+</tileset>
